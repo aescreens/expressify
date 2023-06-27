c4caf6eb65710fb998bd8290852b5dd8b13be574
AE Screens <96313237+aescreens@users.noreply.github.com>
2023-06-27 15:44:30 -0600

    Added readme

--- output/ExpressionLib.js
+++ output/ExpressionLib.js
@@ -2,10 +2,7 @@ var ExpressionLib = (function () {
     var module = {};
 
     module.myExpression =
-    'const a = 1;\r' +
-    'const b = 2;\r' +
-    '\r' +
-    'a * b;';
+
 
     module.newExpression =
     '"Hello world!"';
